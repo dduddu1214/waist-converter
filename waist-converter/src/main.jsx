48bb47233e7a4d6fa4a5d2a9e54c8f1c66c5f263
dduddu1214 <tmfrl1747@naver.com>
2025-05-20 16:23:36 +0900

    ga4 추가

--- waist-converter/src/main.jsx
+++ waist-converter/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+const gaId = import.meta.env.VITE_GA_ID;
+
+if (gaId) {
+  const script1 = document.createElement("script");
+  script1.setAttribute("async", "");
+  script1.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+  document.head.appendChild(script1);
+
+  const script2 = document.createElement("script");
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${gaId}');
+  `;
+  document.head.appendChild(script2);
+}
